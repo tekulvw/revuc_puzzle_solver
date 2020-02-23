2211a89d2370bb106b6d5b6f15f5e3cb9433511c
moonbuffalo <gnick96@gmail.com>
2020-02-23 02:30:20 -0500

    nothing real

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def get_regions(img):
     sizeSmall = (800,600)
     cv.imshow("original", cv.resize(img, (800, 600)))
 
-    imgMedian = cv.medianBlur(img,21)
+    imgMedian = cv.medianBlur(img,31)
 
-    imgGauss = cv.GaussianBlur(img,(31,31),cv.BORDER_DEFAULT)
+    imgGauss = cv.GaussianBlur(img,(51,51),cv.BORDER_DEFAULT)
     #cv.imshow("smooth", cv.resize(imgGauss,sizeSmall))
 
     
@@ -53,18 +53,11 @@ def get_regions(img):
     closing = cv.morphologyEx(sum2, cv.MORPH_CLOSE, (21,21))
     cv.imshow("closed", cv.resize(closing, sizeSmall))
 
+    #lines = cv.HoughLinesP(satedges,1,np.pi/180,100,minLineLength=100,maxLineGap=1)
 
-
-    kernel = np.ones((5,5),np.uint8)
-    erosion = cv.erode(hueedges,kernel,iterations = 1)
-
-    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    edges = cv.Canny(gray,25,150,apertureSize = 5)
-    lines = cv.HoughLinesP(satedges,1,np.pi/180,100,minLineLength=100,maxLineGap=1)
-
-    for line in lines:
-        x1,y1,x2,y2 = line[0]
-        cv.line(satedges,(x1,y1),(x2,y2),(0,255,0),2)
+    ##for line in lines:
+    ##    x1,y1,x2,y2 = line[0]
+    ##    cv.line(satedges,(x1,y1),(x2,y2),(0,255,0),2)
 
     cv.imshow("hue edges", cv.resize(hueedges, sizeSmall))
     cv.imshow("sat edges", cv.resize(satedges, sizeSmall))
@@ -94,10 +87,67 @@ def get_regions(img):
     #v.drawContours(img, contours, -1, (0, 255, 0), 3) 
     #cv.imshow("gauss line", cv.resize(harris, (800, 600)))
 
+def get_regions(img):
+    sizeBig = (1920,1080)
+    sizeSmall = (800,600)
+    cv.imshow("original", cv.resize(img, (800, 600)))
+
+    imgMedian = cv.medianBlur(img,31)
+
+    imgGauss = cv.GaussianBlur(img,(111,111),cv.BORDER_DEFAULT)
+    #cv.imshow("smooth", cv.resize(imgGauss,sizeSmall))
+
+    #imgGray = cv.cvtColor(imgGauss, cv.COLOR_RGB2GRAY)
+
+    #cv.imshow("gray", cv.resize(imgGray, sizeSmall))
+    
+    #imgGrayEdges = cv.Canny(imgGray,40,100)
+    #cv.imshow("grayEdges", cv.resize(imgGrayEdges, sizeSmall))
+
+
+    hsv = cv.cvtColor(imgGauss, cv.COLOR_RGB2HSV)
+    hsv[:, :, 0] = cv.equalizeHist(hsv[:, :, 0])
+    hsv[:, :, 1] = cv.equalizeHist(hsv[:, :, 1])
+    hsv[:, :, 2] = cv.equalizeHist(hsv[:, :, 2])
+
+
+    hue = hsv[:, :, 0]
+    sat = hsv[:, :, 1]
+    val = hsv[:, :, 2]
+    cv.imshow("hue", cv.resize(hue, sizeSmall))
+    cv.imshow("sat", cv.resize(sat, sizeSmall))
+    cv.imshow("val", cv.resize(val, sizeSmall))
+
+    
+
+
+    hueedges = cv.Canny(hue,100,120)
+    satedges = cv.Canny(sat,100,120)
+    valedges = cv.Canny(val,100,120)
+
+    sum1 = cv.bitwise_or(hueedges,satedges)
+    sum2 = cv.bitwise_or(sum1,valedges)
+
+    imgGauss2 = cv.GaussianBlur(sum2,(111,111),cv.BORDER_DEFAULT)
+    cv.imshow("guass2", cv.resize(sum2, sizeSmall))
+
+
+    #lines = cv.HoughLinesP(satedges,1,np.pi/180,100,minLineLength=100,maxLineGap=1)
+
+    #for line in lines:
+    #    x1,y1,x2,y2 = line[0]
+    #    cv.line(satedges,(x1,y1),(x2,y2),(0,255,0),2)
+
+    #cv.imshow("hue edges", cv.resize(hueedges, sizeSmall))
+    #cv.imshow("sat edges", cv.resize(satedges, sizeSmall))
+    #cv.imshow("val edges", cv.resize(valedges, sizeSmall))
+
+    
+
 
 
 def main():
-    img = get_img("mickey_mouse_24.jpg")
+    img = get_img("Lab.jpg")
     get_regions(img)
 
     #cv.imshow("no_bg", cv.resize(img_exp,  (800, 600)))
