207e68f5d0456d59271058207edc01e0caaf7e7b
Will Tekulve <tekulve.will@gmail.com>
2020-02-23 13:38:42 -0500

    maybe corners?

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import cv2 as cv
 import numpy as np
 from pathlib import Path
-from shapely.geometry import Polygon
-from matplotlib import pyplot as plt
 
 
 def resize(img):
-    return cv.resize(img, (1920, 1080))
+    return cv.resize(img, (810, 1080))
 
 
-def remove_bg(img):
+def get_pieces(img):
     # cv.imshow("orig", resize(img))
 
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -26,8 +24,6 @@ def remove_bg(img):
     mask = cv.inRange(hsv, lower_gray, upper_gray)
     mask = cv.medianBlur(mask, 7)
 
-    res = cv.bitwise_and(img, img, mask=mask)
-
     contours, he = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
     # Display the resulting frame
@@ -38,13 +34,51 @@ def remove_bg(img):
 
     pieces = sorted(contours, key=lambda c: cv.contourArea(c), reverse=True)[:4]
 
-    return hsv
+    return pieces
+
+
+def get_corners(piece_contour, length=5, extrema_limit=0.5):
+    ret = []
+
+    slopes = []
+    for i, point in enumerate(piece_contour):
+        next_pt = piece_contour[(i + length) % (len(piece_contour) - 1)]
+        diff = (next_pt - point)[0]
+        angle = np.arctan2(diff[1], diff[0])
+        if angle < 0:
+            angle += 2*np.pi
+        slopes.append(angle)
+
+    normalized_angles = []
+    for i, slope in enumerate(slopes):
+        if i == 0:
+            normalized_angles.append(slope)
+        else:
+            last_slope = normalized_angles[i - 1]
+            next_slope = slopes[(i + 1) % (len(slopes) - 1)]
+            normalized_angles.append(last_slope + np.arctan2(np.sin(next_slope - slope), np.cos(next_slope - slope)))
+
+    derivative = np.gradient(normalized_angles)
+
+    extrema_points = [piece_contour[i] for i, dtheta in enumerate(derivative) if np.abs(dtheta) > extrema_limit]
+
+    # plt.plot(slopes)
+    # plt.plot(derivative)
+    # plt.plot(normalized_angles)
+    # plt.show()
+
+    return extrema_points
 
 
 def main():
     for img_path in (Path.cwd() / 'imgs' / 'flipped').glob("*.jpg"):
         img = cv.imread(str(img_path))
-        no_bg_img = remove_bg(img)
+        pieces = get_pieces(img)
+        corners = [c for p in pieces for c in get_corners(p, length=10, extrema_limit=0.1)]
+
+        cv.drawContours(img, [cnt for p in pieces for cnt in p], -1, (255, 0, 0), thickness=2)
+        cv.drawContours(img, corners, -1, (0, 255, 0), thickness=20)
+        cv.imshow('corners?', resize(img))
 
         break
 
